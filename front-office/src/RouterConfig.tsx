1d397fb8ddb0cbe315359d17c23a31b5c8bd2ae7
yamada-ai <hatsudaaa@gmail.com>
2024-06-19 06:48:16 +0900

    item list mock

--- front-office/src/RouterConfig.tsx
+++ front-office/src/RouterConfig.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App.tsx'
 import Register from './component/Register.tsx';
 import Login from './component/Login.tsx';
+import ItemList from './component/ItemList.tsx';
 
 export const RouterConfig = () => {
     return (
@@ -12,6 +13,7 @@ export const RouterConfig = () => {
                         <Route path='/' element={<App/>} />
                         <Route path="/register" element={<Register />} />
                         <Route path="/login" element={<Login />} />
+                        <Route path="/item-list" element={<ItemList />} />
                         {/* <Route path="/room/:room_id" element={<ChatGUI />} /> */}
                     </Routes>
                 </BrowserRouter>
