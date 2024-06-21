13a644281a7caef7a7db6f210c3e9bd7f62fe32c
hayato mochinaga <hayabusa06mochi@gmail.com>
2024-06-21 11:44:11 +0900

    temp save nav-bar

--- front-office/src/components/layout/Navbar.tsx
+++ front-office/src/components/layout/Navbar.tsx
@@ -2,42 +2,73 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => (
-  <div className="bg-gray-100">
-    <div className="navbar container mx-auto" style={{ width: '80%', height: 'calc(3.25rem * 2.0)'} }>
+  <div className="bg-gray-100 shadow-md"> {/* shadow-md を追加 */}
+    <div className="navbar container mx-auto" style={{ width: '80%', height: 'calc(3.25rem * 2.0)' }}>
       <div className="flex-1 flex items-center">
-        <a className="text-3xl font-poiret font-extrabold">RakuStyle</a>
+        <a className="text-4xl font-poiret font-extrabold">RakuStyle</a>
         <ul className="menu menu-horizontal p-0 ml-4">
-        <li><Link to="/item-list/set" className="text-lg">Sets</Link></li>
-        <li><Link to="/item-list/top" className="text-lg">Tops</Link></li>
-        <li><Link to="/item-list/bottom" className="text-lg">Bottom</Link></li>
+          <li><Link to="/item-list/set" className="text-lg">Sets</Link></li>
+          <li><Link to="/item-list/top" className="text-lg">Tops</Link></li>
+          <li><Link to="/item-list/bottom" className="text-lg">Bottom</Link></li>
         </ul>
       </div>
 
-      <div className="dropdown dropdown-end">
-        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-          <div className="indicator">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-            </svg>
-            <span className="badge badge-sm indicator-item">1</span>
+      {/* 検索フォームの追加 */}
+      <div className="flex items-center ml-auto">
+        <form className="flex gap-3">
+          <div className="flex">
+            <input
+              type="text"
+              placeholder="商品名で検索"
+              className="w-full md:w-60 px-3 h-10 rounded-l-md border-2 border-gray-600 focus:outline-none focus:border-gray-600"
+            />
+            <button
+              type="submit"
+              className="bg-gray-600 text-white rounded-r-md px-2 md:px-3 py-0 md:py-1"
+            >
+              Search
+            </button>
           </div>
-        </div>
-        <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
-          <div className="card-body">
-            <span className="font-bold text-lg">8 Items</span>
-            <span className="text-info">Subtotal: $999</span>
-            <div className="card-actions">
-              <button className="btn btn-primary btn-block">View cart</button>
+          <select
+            id="pricingType"
+            name="pricingType"
+            className="w-27 h-10 border-2 border-gray-600 focus:outline-none focus:border-gray-600 text-black rounded-md px-2 md:px-3 py-0 md:py-1 tracking-wider"
+          >
+            <option value="All" selected>
+              All
+            </option>
+            <option value="set">set</option>
+            <option value="tops">tops</option>
+            <option value="bottoms">bottoms</option>
+          </select>
+        </form>
+
+        {/* カートアイコンとその内容 */}
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+              </svg>
+              <span className="badge badge-sm indicator-item">1</span>
+            </div>
+          </div>
+          <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+            <div className="card-body">
+              <span className="font-bold text-lg">8 Items</span>
+              <span className="text-info">Subtotal: $999</span>
+              <div className="card-actions">
+                <button className="btn btn-primary btn-block">View cart</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="flex-none gap-2">
+        {/* ユーザーアイコンとその内容 */}
         <div className="dropdown dropdown-end">
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
             <div className="w-10 rounded-full">
-              <img alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              <img alt="User Avatar" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
             </div>
           </div>
           <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
