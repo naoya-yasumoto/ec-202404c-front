14af1e53b72bb512d1818de9b2a31ddcc0f26e15
hayato mochinaga <hayabusa06mochi@gmail.com>
2024-06-21 17:53:55 +0900

    pluginをインストールして、pagenationまで一旦完成。

--- front-office/src/components/Item.tsx
+++ front-office/src/components/Item.tsx
@@ -12,7 +12,7 @@ const Item: React.FC<ItemProps> = ({ item }) => {
   };
 
   return (
-    <Link to={`/item/${item.id}`} className="relative max-w-[19%] rounded overflow-hidden shadow-lg m-4 border-2 border-gray-300 transition ease-out duration-500 hover:border-gray-500 hover:shadow-lg group block">
+    <Link to={`/item/${item.id}`} className="relative max-w-[19%] rounded overflow-hidden shadow-lg m-4 border-2 border-gray-300 transition ease-out duration-500 hover:border-gray-600 hover:shadow-lg group block">
       <img className="w-full h-auto object-cover" src={`http://${HOST_IP}:9090/img/` + item.imagePath} alt={item.name} />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{item.name}</div>
@@ -22,7 +22,7 @@ const Item: React.FC<ItemProps> = ({ item }) => {
         {/* Detailボタンを削除 */}
       </div>
       <div className="py-6"></div> {/* 下部の余白を保持 */}
-      <button onClick={handleCardClick} className="transform -translate-x-1/2 translate-y-0 w-[60%] rounded-[1rem] bg-gray-500 hover:bg-gray-700 text-white text-[1rem] py-2 px-4 absolute left-1/2 bottom-4 opacity-0 transition ease-out duration-300 group-hover:translate-y-0 group-hover:opacity-100">
+      <button onClick={handleCardClick} className="transform -translate-x-1/2 translate-y-0 w-[60%] h-[10%] rounded-[0.5rem] bg-gray-600 hover:bg-gray-800 text-white text-[1rem] py-2 px-4 absolute left-1/2 bottom-4 opacity-0 transition ease-out duration-300 group-hover:translate-y-0 group-hover:opacity-100">
         Show Detail
       </button>
     </Link>
--- front-office/src/components/ItemCardList.tsx
+++ front-office/src/components/ItemCardList.tsx
@@ -5,8 +5,8 @@ import axios from 'axios';
 import { HOST_IP } from '../config';
 
 const ItemCardList: React.FC = () => {
-  const { type } = useParams();
-  const [items, setItems] = useState([]);
+  const { type } = useParams<{ type: string }>();
+  const [items, setItems] = useState<any[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 3;
 
@@ -37,18 +37,68 @@ const ItemCardList: React.FC = () => {
           <Item key={index} item={item} />
         ))}
       </div>
-      {/* ページネーションの表示 */}
-      <nav aria-label="Pagination">
-        <ul className="pagination">
-          {items.length > itemsPerPage &&
-            Array.from({ length: Math.ceil(items.length / itemsPerPage) }, (_, index) => (
-              <li key={index} className={currentPage === index + 1 ? 'active' : ''}>
-                <button className="pagination-link" onClick={() => paginate(index + 1)}>
-                  {index + 1}
-                </button>
-              </li>
-            ))}
-        </ul>
+      {/* プラグインページネーションの表示 */}
+      <nav className="flex items-center gap-4 justify-center mt-4">
+        <button
+          disabled={currentPage === 1}
+          onClick={() => setCurrentPage(currentPage - 1)}
+          className={`flex items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-gray-900/20 ${
+            currentPage === 1 ? 'disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none' : ''
+          }`}
+          type="button"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            aria-hidden="true"
+            className="w-4 h-4"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"></path>
+          </svg>
+          Previous
+        </button>
+        <div className="flex items-center gap-2">
+          {Array.from({ length: Math.ceil(items.length / itemsPerPage) }, (_, index) => (
+            <button
+              key={index}
+              onClick={() => paginate(index + 1)}
+              className={`relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase transition-all hover:bg-gray-900/10 active:bg-gray-900/20 ${
+                currentPage === index + 1
+                  ? 'bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none'
+                  : 'text-gray-900'
+              }`}
+              type="button"
+            >
+              <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">{index + 1}</span>
+            </button>
+          ))}
+        </div>
+        <button
+          disabled={currentPage === Math.ceil(items.length / itemsPerPage)}
+          onClick={() => setCurrentPage(currentPage + 1)}
+          className={`flex items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-gray-900/20 ${
+            currentPage === Math.ceil(items.length / itemsPerPage)
+              ? 'disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none'
+              : ''
+          }`}
+          type="button"
+        >
+          Next
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            aria-hidden="true"
+            className="w-4 h-4"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"></path>
+          </svg>
+        </button>
       </nav>
     </div>
   );
