0b6a5d0798143d9a8a35f3954364440bff56ddc2
noy-ing <naoya20011102@icloud.com>
2024-06-21 16:39:32 +0900

    Register.tsxスタイル変更後

--- front-office/src/components/Register.tsx
+++ front-office/src/components/Register.tsx
@@ -3,11 +3,8 @@ import axios from "axios";
 import { useForm, Controller } from "react-hook-form";
 import MySelect from "./MySelect";
 import { prefecturesOptions } from "../utils/prefectures";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { validationSchema } from '../utils/validationSchema';
-import { HOST_IP } from '../config';
+import { HOST_IP } from "../config";
 import { useNavigate } from "react-router-dom";
-
 
 interface SignUpForm {
   lastName: string;
@@ -20,8 +17,6 @@ interface SignUpForm {
   address: string;
   tel: string;
 }
-
-  
 
 const Register: React.FC = () => {
   const {
@@ -36,20 +31,12 @@ const Register: React.FC = () => {
     // resolver: zodResolver(validationSchema),
   });
   const [loading, setLoading] = useState(false);
-
-
   const navigate = useNavigate();
-  
-
 
   const onSubmit = async (data: SignUpForm) => {
     const combinedName = `${data.lastName} ${data.firstName}`;
-    console.log("combinedName:" + combinedName);
-    console.log("data.prefectures:" + data.prefectures);
-    
-    
 
-    // 結合したフィールドを含むオブジェクトを作成
+    // Create form data object with combined fields
     const formData = {
       name: combinedName,
       email: data.email,
@@ -58,28 +45,27 @@ const Register: React.FC = () => {
       prefecture: data.prefectures,
       municipalities: data.municipalities,
       address: data.address,
-      telephone: data.tel
+      telephone: data.tel,
     };
-    
-    
     console.log(formData);
+
     try {
-      const response = await axios.post(`http://${HOST_IP}:8080/ec-202404c/users/register`, formData);
-      if(response.status === 201){
-          navigate('/login');
-         }
-      console.log('Employee data:', response.data);
-    } catch (error:any) {
-      console.log("catch:" + error.response.status);
+      const response = await axios.post(
+        `http://${HOST_IP}:8080/ec-202404c/users/register`,
+        formData
+      );
+      if (response.status === 201) {
+        navigate("/login");
+      }
+      console.log("Employee data:", response.data);
+    } catch (error: any) {
+      console.error("An error occurred:", error);
       if (error.response && error.response.status >= 500) {
-        // サーバーエラーの場合
-        console.log("500:" + error.response.status);
+        console.log("500:", error.response.status);
       } else {
-        // その他のエラーの場合は適切な処理を行う
-        console.error('An error occurred:', error);
+        console.error("An error occurred:", error);
       }
     }
-    
   };
 
   const fetchAddress = async (postcode: number) => {
@@ -106,89 +92,181 @@ const Register: React.FC = () => {
   };
 
   return (
-    <div className="form-container">
-      <h1>登録フォーム</h1>
-      <hr />
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="lastName">姓</label>
-        <input type="text" id="lastName" {...register("lastName")}></input>
-        <label htmlFor="firstName">名</label>
-        <input type="text" id="firstName" {...register("firstName")}></input>
-        <br />
-        <p>{errors.lastName && errors.lastName?.message}</p>
-        <p>{errors.firstName && errors.firstName?.message}</p>
-        <br />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full sm:w-4/5 lg:w-3/5">
+        <div className="mx-2 my-20 sm:my-auto">
+          <div className="flex justify-center">
+            <div className="w-full sm:w-11/12 p-12 sm:px-10 sm:py-6 bg-white rounded-lg shadow-md lg:shadow-lg">
+              <h2 className="text-center  font-semibold text-3xl lg:text-4xl text-gray-800 mt-6 mb-6" >
+                Sign Up
+              </h2>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="flex">
+                  <div className="w-1/2 pr-2">
+                    <label
+                      htmlFor="lastName"
+                      className="block text-xs font-semibold text-gray-600 uppercase mt-4"
+                    >
+                      姓
+                    </label>
+                    <input
+                      type="text"
+                      id="lastName"
+                      {...register("lastName")}
+                      className="block w-full py-3 px-1 mt-2 text-gray-800 appearance-none border-b-2 border-gray-100 focus:text-gray-500 focus:outline-none focus:border-gray-200"
+                    />
+                    <p>{errors.lastName && errors.lastName.message}</p>
+                  </div>
+                  <div className="w-1/2 pl-2">
+                    <label
+                      htmlFor="firstName"
+                      className="block text-xs font-semibold text-gray-600 uppercase mt-4"
+                    >
+                      名
+                    </label>
+                    <input
+                      type="text"
+                      id="firstName"
+                      {...register("firstName")}
+                      className="block w-full py-3 px-1 mt-2 text-gray-800 appearance-none border-b-2 border-gray-100 focus:text-gray-500 focus:outline-none focus:border-gray-200"
+                    />
+                    <p>{errors.firstName && errors.firstName.message}</p>
+                  </div>
+                </div>
 
-        <label htmlFor="email">メールアドレス</label>
-        <input type="email" id="email" {...register("email")}></input>
-        <p>{errors.email && errors.email?.message}</p>
-        <br />
+                <label
+                  htmlFor="email"
+                  className="block text-xs font-semibold text-gray-600 uppercase mt-4"
+                >
+                  メールアドレス
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  {...register("email")}
+                  className="block w-full py-3 px-1 mt-2 text-gray-800 appearance-none border-b-2 border-gray-100 focus:text-gray-500 focus:outline-none focus:border-gray-200"
+                />
+                <p>{errors.email && errors.email.message}</p>
 
-        <label htmlFor="password">パスワード</label>
-        <input type="password" id="password" {...register("password")}></input>
-        <p>{errors.password && errors.password?.message}</p>
-        <br />
+                <label
+                  htmlFor="password"
+                  className="block text-xs font-semibold text-gray-600 uppercase mt-4"
+                >
+                  パスワード
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  {...register("password")}
+                  className="block w-full py-3 px-1 mt-2 mb-4 text-gray-800 appearance-none border-b-2 border-gray-100 focus:text-gray-500 focus:outline-none focus:border-gray-200"
+                />
+                <p>{errors.password && errors.password.message}</p>
 
-        <label htmlFor="postcode">郵便番号</label>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <input type="number" id="postcode" {...register("postcode")} />
-          <button
-            type="button"
-            onClick={() => fetchAddress(watch("postcode"))}
-            disabled={loading}
-          >
-            {loading ? "取得中..." : "住所取得"}
-          </button>
+                <label
+                  htmlFor="postcode"
+                  className="block text-xs font-semibold text-gray-600 uppercase mt-4"
+                >
+                  郵便番号
+                </label>
+                <div className="flex items-center">
+                  <input
+                    type="number"
+                    id="postcode"
+                    {...register("postcode")}
+                    className="block w-full py-3 px-1 mt-2 text-gray-800 appearance-none border-b-2 border-gray-100 focus:text-gray-500 focus:outline-none focus:border-gray-200"
+                  />
+                  <button
+                    type="button"
+                    onClick={() => fetchAddress(watch("postcode"))}
+                    disabled={loading}
+                    className="ml-4 w-48 bg-gray-800 py-3 px-6 rounded-sm text-white uppercase font-medium focus:outline-none hover:bg-gray-700 hover:shadow-none"
+                  >
+                    {loading ? "取得中..." : "住所取得"}
+                  </button>
+                </div>
+                <p>{errors.postcode && errors.postcode.message}</p>
+
+                <label
+                  htmlFor="prefectures"
+                  className="block text-xs font-semibold text-gray-600 uppercase mt-4"
+                >
+                  都道府県
+                </label>
+                <Controller
+                  name="prefectures"
+                  control={control}
+                  render={({ field }) => (
+                    <MySelect
+                      value={field.value}
+                      onChange={field.onChange}
+                      options={prefecturesOptions}
+                    />
+                  )}
+                />
+
+                <label
+                  htmlFor="municipalities"
+                  className="block text-xs font-semibold text-gray-600 uppercase mt-4"
+                >
+                  市区町村
+                </label>
+                <input
+                  type="text"
+                  id="municipalities"
+                  {...register("municipalities")}
+                  className="block w-full py-3 px-1 mt-2 text-gray-800 appearance-none border-b-2 border-gray-100 focus:text-gray-500 focus:outline-none focus:border-gray-200"
+                />
+                <p>{errors.municipalities && errors.municipalities.message}</p>
+
+                <label
+                  htmlFor="address"
+                  className="block text-xs font-semibold text-gray-600 uppercase mt-4"
+                >
+                  住所
+                </label>
+                <input
+                  type="text"
+                  id="address"
+                  {...register("address")}
+                  className="block w-full py-3 px-1 mt-2 text-gray-800 appearance-none border-b-2 border-gray-100 focus:text-gray-500 focus:outline-none focus:border-gray-200"
+                />
+                <p>{errors.address && errors.address.message}</p>
+
+                <label
+                  htmlFor="tel"
+                  className="block text-xs font-semibold text-gray-600 uppercase mt-4"
+                >
+                  電話番号
+                </label>
+                <input
+                  type="tel"
+                  id="tel"
+                  inputMode="numeric"
+                  {...register("tel")}
+                  maxLength={11}
+                  className="block w-full py-3 px-1 mt-2 mb-4 text-gray-800 appearance-none border-b-2 border-gray-100 focus:text-gray-500 focus:outline-none focus:border-gray-200"
+                />
+                <p>{errors.tel && errors.tel.message}</p>
+
+                <div className="flex space-x-4 mt-8">
+                  <button
+                    type="submit"
+                    className="w-full py-3 bg-gray-800 rounded-sm font-medium text-white uppercase focus:outline-none hover:bg-gray-700 hover:shadow-none"
+                  >
+                    登録
+                  </button>
+                  <button
+                    type="reset"
+                    className="w-full py-3 bg-gray-300 rounded-sm font-medium text-gray-800 uppercase focus:outline-none hover:bg-gray-400 hover:shadow-none"
+                  >
+                    キャンセル
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-        <p>{errors.postcode && errors.postcode?.message}</p>
-        <br />
-
-        <label htmlFor="prefectures">都道府県</label>
-        <Controller
-          name="prefectures"
-          control={control}
-          render={({ field }) => (
-            <MySelect
-              value={field.value}
-              onChange={field.onChange}
-              options={prefecturesOptions}
-            />
-          )}
-          
-        />
-        
-        {/* <p>{errors.prefectures && errors.prefectures?.message}</p>  */}
-       
-        <br />
-
-        <label htmlFor="municipalities">市区町村</label>
-        <input
-          type="text"
-          id="municipalities"
-          {...register("municipalities")}
-        ></input>
-        <p>{errors.municipalities && errors.municipalities?.message}</p>
-        <br />
-
-        <label htmlFor="address">住所</label>
-        <input type="text" id="address" {...register("address")}></input>
-        <p>{errors.address && errors.address?.message}</p>
-        <br />
-
-        <label htmlFor="tel">電話番号</label>
-        <input
-          type="tel"
-          id="tel"
-          inputMode="numeric"
-          {...register("tel")}
-          maxLength={11}
-        ></input>
-        <p>{errors.tel && errors.tel?.message}</p>
-        <br />
-
-        <button type="submit">登録</button>
-        <button type="reset">キャンセル</button>
-      </form>
+      </div>
     </div>
   );
 };
