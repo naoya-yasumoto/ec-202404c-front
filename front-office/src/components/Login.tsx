ef75cd83738abb7b0cf21207a665dc14c53d8b34
noy-ing <naoya20011102@icloud.com>
2024-06-24 08:58:16 +0900

    loginスタイル変更

--- front-office/src/components/Login.tsx
+++ front-office/src/components/Login.tsx
@@ -1,63 +1,65 @@
-import React from 'react';
-import axios from 'axios';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { loginSchema } from '../utils/loginSchema';
-import { useNavigate } from 'react-router-dom';
-import { HOST_IP } from '../config';
+import { loginSchema } from "../utils/loginSchema";
+import { useNavigate } from "react-router-dom";
+import { HOST_IP } from "../config";
 
 interface SignUpForm {
-    
-    email: string,
-    password: string,
-    postcode: number
-  }
+  email: string;
+  password: string;
+  postcode: number;
+}
 
 const Login: React.FC = () => {
-    const { register, handleSubmit, formState: { errors } } 
-    = useForm<SignUpForm>({mode:"onBlur", resolver: zodResolver(loginSchema)});
-  
-    const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpForm>({
+    mode: "onBlur",
+    resolver: zodResolver(loginSchema),
+  });
+
+  const navigate = useNavigate();
 
   const onSubmit = async (data: SignUpForm) => {
-    
-
     // 結合したフィールドを含むオブジェクトを作成
     const formData = {
       email: data.email,
       password: data.password,
-      
     };
 
     //ここにjson送信を入れる
     //const response = await axios.post('http://192.168.16.175:8080/ec-202404c/auth/login', formData);
-    const response = await axios.post(`http://${HOST_IP}:8080/ec-202404c/auth/login`, formData);
+    const response = await axios.post(
+      `http://${HOST_IP}:8080/ec-202404c/auth/login`,
+      formData
+    );
 
     // 成功
-    if(response.status === 200){
-      navigate('/item-list/set');
-    }else{
-      <p>エラーが発生しました！</p>
+    if (response.status === 200) {
+      navigate("/item-list/set");
+    } else {
+      <p>エラーが発生しました！</p>;
     }
 
-
-    
     console.dir("response:" + JSON.stringify(response));
     const accessToken = response.headers["access-token"];
 
     // アクセストークンをセッションストレージに格納
-    window.sessionStorage.setItem('accessToken', accessToken);
-    const token = window.sessionStorage.getItem('accessToken');
+    window.sessionStorage.setItem("accessToken", accessToken);
+    const token = window.sessionStorage.getItem("accessToken");
 
     if (token) {
       // デコードしてユーザー名を取得
-      const tokenPayload = JSON.parse(atob(token.split('.')[1]));
+      const tokenPayload = JSON.parse(atob(token.split(".")[1]));
       const username = tokenPayload.username;
       const userid = tokenPayload.userid;
 
       // 必要に応じて username を使用する
-  }
-  
+    }
 
     ///アクセスが必要なものに記述する
     // セッションストレージに格納したアクセストークンを取得する方法
@@ -74,28 +76,19 @@ const Login: React.FC = () => {
     //   );
     //   // 取得したJSONデータをコンソールに出力して確認
     //   console.dir("response:" + JSON.stringify(response));
-
-
-
   };
-  
-  
 
   return (
-    
-
     //sample
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 ">
       <div className="w-full sm:w-4/5 lg:w-3/5 mt-20 mb-20">
         <div className="mx-2 my-20 sm:my-auto">
           <div className="flex justify-center">
             <div className="w-full sm:w-11/12 p-12 sm:px-10 sm:py-6 bg-white rounded-lg shadow-md lg:shadow-lg">
-              <h2 className="text-center  font-semibold text-3xl lg:text-4xl text-gray-800 mt-6 mb-6" >
+              <h2 className="text-center  font-semibold text-3xl lg:text-4xl text-gray-800 mt-6 mb-6">
                 Login
               </h2>
               <form onSubmit={handleSubmit(onSubmit)}>
-                
-
                 <label
                   htmlFor="email"
                   className="block text-xs font-semibold text-gray-600 uppercase mt-4"
@@ -124,21 +117,22 @@ const Login: React.FC = () => {
                 />
                 <p>{errors.password && errors.password.message}</p>
 
-                
-
-                <div className="flex space-x-4 mt-8">
-                  <button
-                    type="submit"
-                    className="w-full py-3 bg-gray-800 rounded-sm font-medium text-white uppercase focus:outline-none hover:bg-gray-700 hover:shadow-none"
-                  >
-                    登録
-                  </button>
-                  <button
-                    type="reset"
-                    className="w-full py-3 bg-gray-300 rounded-sm font-medium text-gray-800 uppercase focus:outline-none hover:bg-gray-400 hover:shadow-none"
-                  >
-                    キャンセル
-                  </button>
+                <button
+                  type="submit"
+                  className="w-full py-3 mt-4 bg-gray-800 rounded-sm font-medium text-white uppercase focus:outline-none hover:bg-gray-700 hover:shadow-none"
+                >
+                  ログイン
+                </button>
+                <div className="sm:flex sm:flex-wrap mt-8 sm:mb-4 text-sm text-center">
+                  <a href="#" className="flex-2 underline">
+                    Forgot password?
+                  </a>
+                  <p className="flex-1 text-gray-500 text-md mx-4 my-1 sm:my-auto">
+                    or
+                  </p>
+                  <a href="#" className="flex-2 underline">
+                    Create an Account
+                  </a>
                 </div>
               </form>
             </div>
@@ -146,7 +140,7 @@ const Login: React.FC = () => {
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Login;
