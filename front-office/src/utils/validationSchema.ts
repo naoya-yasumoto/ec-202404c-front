d1efa0f7594c9b313a208317b9d696c08415c66e
nooum39 <mikantaruto1380@gmail.com>
2024-06-24 13:39:29 +0900

    メールアドレスの重複チェック

--- front-office/src/utils/validationSchema.ts
+++ front-office/src/utils/validationSchema.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 const phoneRegex = /^0\d{9,10}$/;
 
 export const validationSchema = z.object({
-  firstName: z.string().min(1,{message: "性は必須です"}),
-  lastName: z.string().min(1,{message: "名は必須です"}),
+  firstName: z.string().min(1,{message: "名は必須です"}),
+  lastName: z.string().min(1,{message: "姓は必須です"}),
   email: z.string().min(1,{message: "メールアドレスは必須です"}).email({message: "正しいメールアドレスを入力してください"}),
   password: z.string().min(1,{message: "パスワードは必須です"}).min(8,{message: "パスワードは8文字以上で入力してください"}),
   postcode: z.coerce.number().min(1,{message: "郵便番号は必須です"}),
