af32529692afa13768e074956157ec0ddc8a1549
noy-ing <naoya20011102@icloud.com>
2024-06-24 14:21:57 +0900

    mochiに託した

--- front-office/src/pages/ItemListAll.tsx
+++ front-office/src/pages/ItemListAll.tsx
@@ -6,15 +6,26 @@ import Footer from '../components/layout/Footer';
 import { HOST_IP } from '../config';
 import ItemCardList from '../components/ItemCardList';
 
-const ItemListAll: React.FC = () => {
+const ItemList: React.FC = () => {
   const { type } = useParams<{ type: string }>(); // useParamsの型指定
-  const [items, setItems] = useState<any[]>([]); // itemsの型指定
+  const [itemsSet, setItemsSet] = useState<any[]>([]); // setアイテムのステート
+  const [itemsTop, setItemsTop] = useState<any[]>([]); // topアイテムのステート
+  const [itemsBottom, setItemsBottom] = useState<any[]>([]); // bottomアイテムのステート
 
   useEffect(() => {
     const getItemsAsync = async () => {
       try {
-        const response = await axios.get(`http://${HOST_IP}:8080/ec-202404c/items/${type}`);
-        setItems(response.data.items);
+        // 各タイプの商品を取得するためのリクエストを並行して送信する
+        const [responseSet, responseTop, responseBottom] = await Promise.all([
+          axios.get(`http://${HOST_IP}:8080/ec-202404c/items/set`),
+          axios.get(`http://${HOST_IP}:8080/ec-202404c/items/top`),
+          axios.get(`http://${HOST_IP}:8080/ec-202404c/items/bottom`),
+        ]);
+
+        // レスポンスから商品リストをセットする
+        setItemsSet(responseSet.data.items);
+        setItemsTop(responseTop.data.items);
+        setItemsBottom(responseBottom.data.items);
       } catch (error) {
         console.error('Error fetching items:', error);
         // エラー処理を追加することも考慮する
@@ -22,19 +33,21 @@ const ItemListAll: React.FC = () => {
     };
 
     getItemsAsync();
-  }, [type]);
+  }, []);
 
   return (
     <div style={{ width: "100%" }}>
       <div>
         {/* <Navbar /> */}
-        {type === 'set' && <ItemCardList items={items} />} {/* "set"の場合 */}
-        {type === 'top' && <ItemCardList items={items} />} {/* "top"の場合 */}
-        {type === 'bottom' && <ItemCardList items={items} />} {/* "bottom"の場合 */}
+        <div className="flex flex-col gap-8">
+          <ItemCardList items={itemsSet} />
+          <ItemCardList items={itemsTop} />
+          <ItemCardList items={itemsBottom} />
+        </div>
         <Footer />
       </div>
     </div>
   );
 };
 
-export default ItemListAll;
+export default ItemList;
