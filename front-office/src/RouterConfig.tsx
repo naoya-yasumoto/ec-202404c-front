9501dcb698f817313c328f305239e8b69022ef4d
yamada-ai <hatsudaaa@gmail.com>
2024-06-26 00:36:23 +0900

    can controll navbar by using location

--- front-office/src/RouterConfig.tsx
+++ front-office/src/RouterConfig.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from 'react';
 import { ECsiteContext, CartItem } from './contexts.ts';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import Register from './components/Register.tsx';
 import Login from './components/Login.tsx';
 import ItemList from './pages/ItemList.tsx';
@@ -14,27 +14,43 @@ import Navbar from './components/layout/Navbar.tsx';
 import ItemListAll from './pages/ItemListAll.tsx';
 import Favorite from './pages/Favorite.tsx';
 
-export const RouterConfig = () => {
+const AppLayout = ({ children }) => {
+    const { pathname } = useLocation();
     const [username, setUsername] = useState<any>(null);
+
+    const showNavbar = !['/login', '/register'].includes(pathname);
+
+    return (
+        <>
+            {showNavbar && <Navbar username={username} setUsername={setUsername} />}
+            {children}
+        </>
+    );
+};
+
+export const RouterConfig = () => {
     const [cartItems, setCartItems] = useState<CartItem[]>([]);
+    const [username, setUsername] = useState<any>(null);
+
     return (
         <>
             <BrowserRouter>
                 <ECsiteContext.Provider value={{cartItems, setCartItems}}>
-                <Navbar username={username} setUsername={setUsername} />
-                <Routes>
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/login" element={<Login setUsername={setUsername} />} />
-                    <Route path="/cart" element={<Cart />} />
-                    <Route path="/item-list/:type" element={<ItemList />} />
-                    <Route path="/item-list" element={<ItemListAll />} />
-                    <Route path="/item/:id" element={<ItemDetail />} />
-                    <Route path="/order_confirm" element={<OrderConfirm />} />
-                    <Route path="/credit-card-info" element={<CreditCardInfo />} />
-                    <Route path="/complete" element={<OrderComplete />} />
-                    <Route path="/favorite" element={<Favorite />} />
-                    <Route path="*" element={<NotFound />} /> {/* 404ページへのルート */}
-                </Routes>
+                    <AppLayout>
+                        <Routes>
+                            <Route path="/register" element={<Register />} />
+                            <Route path="/login" element={<Login setUsername={setUsername}/>} />
+                            <Route path="/cart" element={<Cart />} />
+                            <Route path="/item-list/:type" element={<ItemList />} />
+                            <Route path="/item-list" element={<ItemListAll />} />
+                            <Route path="/item/:id" element={<ItemDetail />} />
+                            <Route path="/order_confirm" element={<OrderConfirm />} />
+                            <Route path="/credit-card-info" element={<CreditCardInfo />} />
+                            <Route path="/complete" element={<OrderComplete />} />
+                            <Route path="/favorite" element={<Favorite />} />
+                            <Route path="*" element={<NotFound />} /> {/* 404ページへのルート */}
+                        </Routes>
+                    </AppLayout>
                 </ECsiteContext.Provider>
             </BrowserRouter>
         </>
