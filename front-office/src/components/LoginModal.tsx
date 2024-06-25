ea5a80eb49c27ef407c399b7349fc164bcfcbe4a
hayato mochinaga <hayabusa06mochi@gmail.com>
2024-06-25 15:45:17 +0900

    すべての要素より前面にモーダルを表示できるようにして、スタイルもあてた。ログインボタンを押したらモーダルを表示にするようにもできてるよ

--- front-office/src/components/LoginModal.tsx
+++ front-office/src/components/LoginModal.tsx
@@ -1,4 +1,5 @@
 // LoginModal.tsx
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -8,21 +9,44 @@ interface LoginModalProps {
 }
 
 const LoginModal: React.FC<LoginModalProps> = ({ show, onClose }) => {
+  if (!show) {
+    return null; // モーダルが非表示の場合、nullを返して描画をスキップ
+  }
+
+  const handleLoginButtonClick = () => {
+    onClose(); // ログインボタンがクリックされたらモーダルを閉じる
+  };
+
   return (
-    <div className={`fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 transition-opacity duration-300 ${show ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
-      <div className="bg-white p-8 rounded-lg shadow-lg relative">
+    <div className={`fixed inset-0 flex items-center justify-center bg-black bg-opacity-55 transition-opacity duration-300 opacity-100`} style={{ zIndex: 9999 }}>
+      <div className="bg-white p-10 rounded-lg shadow-lg relative" style={{ width: '38%', height: '31%' }}>
         <h2 className="text-2xl font-semibold mb-4">ログインしてください</h2>
         <p>この機能を利用するにはログインが必要です。</p>
         <div className="mt-4">
-          <Link to="/login" className="text-blue-500 hover:underline">
-            ログイン
+          <Link to="/login">
+            <button onClick={handleLoginButtonClick} className="bg-blue-gray-600 text-white py-2 px-4 rounded hover:bg-blue-gray-900 mt-4" style={{ width: '34%', height: '30%' }}>
+              ログイン
+            </button>
           </Link>
         </div>
         <button
           onClick={onClose}
-          className="absolute top-0 right-0 mt-4 mr-4 text-gray-500 hover:text-gray-700"
+          className="btn btn-square btn-outline absolute top-0 right-0 mt-8 mr-8"
         >
-          ×
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
         </button>
       </div>
     </div>
--- front-office/src/config.ts
+++ front-office/src/config.ts
@@ -1,3 +1,3 @@
 // export const HOST_IP = import.meta.env.VITE_HOST_IP;
-export const HOST_IP = "192.168.16.175";
+export const HOST_IP = "192.168.16.130";
 
