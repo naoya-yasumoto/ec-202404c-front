709c462b9c231c8616d9d932cca4890482c3ae72
nooum39 <mikantaruto1380@gmail.com>
2024-06-25 15:50:07 +0900

    demo修正

--- front-office/src/config.ts
+++ front-office/src/config.ts
@@ -1,6 +1,2 @@
 // export const HOST_IP = import.meta.env.VITE_HOST_IP;
-<<<<<<< HEAD
 export const HOST_IP = "192.168.16.133";
-=======
-export const HOST_IP = "192.168.16.232";
->>>>>>> 7f2adb3c11f1a9cd4b15d9ac53a9601b7ad82085
