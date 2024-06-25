73755d210a88fbb96cac2117c18bca90031d7c02
noy-ing <naoya20011102@icloud.com>
2024-06-25 09:45:06 +0900

    paymentMethodのvalidation追加

--- front-office/src/utils/orderSchema.ts
+++ front-office/src/utils/orderSchema.ts
@@ -10,5 +10,5 @@ export const orderSchema = z.object({
   telephone: z.string().regex(/^\d{10,11}$/, "電話番号は10桁または11桁の数字で入力してください"),
   deliveryDate: z.string().nonempty("配達日時を選択してください"),
   deliveryTime: z.string().nonempty("配達時間を選択してください"),
-  //paymentMethod: z.string().nonempty("お支払い方法を選択してください"),
+  paymentMethod: z.string().nonempty({ message: '選択肢を選んでください' }),
 });
