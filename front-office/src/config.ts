7849743633e7c6289239aff3ff43ed93351a7d42
yamada-ai <hatsudaaa@gmail.com>
2024-06-26 17:36:33 +0900

    bit fix

--- front-office/src/config.ts
+++ front-office/src/config.ts
@@ -1,7 +1,2 @@
 // export const HOST_IP = import.meta.env.VITE_HOST_IP;
-<<<<<<< HEAD
 export const HOST_IP = "192.168.16.175";
-=======
-export const HOST_IP = "192.168.128.189";
-
->>>>>>> origin/feature/nav-bar
