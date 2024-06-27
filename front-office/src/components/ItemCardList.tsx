60d3515d90f9b4269093904eeb44aece7d04634b
hayato mochinaga <hayabusa06mochi@gmail.com>
2024-06-27 10:26:39 +0900

    fixed

--- front-office/src/components/ItemCardList.tsx
+++ front-office/src/components/ItemCardList.tsx
@@ -26,7 +26,7 @@ const ItemCardList: React.FC<{ items: any[]; type: string }> = ({
               <span className="text-orange-800" style={{ marginLeft:'4.5px' }}>.</span>
               {currentItems.length === 0 && (
                 <div className="flex justify-center">
-                  <div className="my-20">検索結果が見つかりませんでした。</div>
+                  <div className="my-20 text-3xl text-blue-gray-800">検索結果が見つかりませんでした。</div>
                 </div>
               )}
             </div>
